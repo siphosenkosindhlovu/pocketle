211b9ba232167fc304089db74abb1386f434760a
Siphosenkosi Ndhlovu <sipho.ndhlovu@outlook.com>
2020-05-22 12:38:12 +0200

    deleted stuff

--- dist (5)/dist/js/countrySelect.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-/*! project-name v0.0.1 | (c) 2020 Siphosenkosi Ndhlovu | MIT License | http://link-to-your-git-repo.com */
-//Initialising country selector
-$("#country_selector").countrySelect({
-  defaultCountry: "jp",
-  responsiveDropdown: true
-});
